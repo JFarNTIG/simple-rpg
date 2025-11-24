f331989372e5bb5044c2aa1a89d973dccf065410
GalnaRobin <robin.dahl@elev.ntig>
2025-11-24 09:44:47 +0100

    Fixat så att det finns en grej som aktiveras när man når slutet på vänster sida

--- src/state/maingame.cpp
+++ src/state/maingame.cpp
@@ -40,6 +40,12 @@ void MainGame::Update(sf::RenderWindow* window) {
     this->map->CheckCollision(this->player);
     this->camera->Update(window, this->map, sf::Vector2f(this->player->getPosition().x, this->player->getPosition().y));
 
+    if(this->player->getPosition().x > 1510.0f) {
+        std::cout << "You Win!" << std::endl;
+        // next map code here
+        MapLoad(this->map, "data/map/level2.json");
+    }
+
     // Reloading of the map
     if(inputManager.IsPressed(InputManager::LoadMap)
     && inputManager.IsKeyBoardPressed(sf::Keyboard::LControl)) {
